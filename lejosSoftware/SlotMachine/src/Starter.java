f6172ffb8ba5d7cd873d55482cd73a4c3540f820
Stephen Fox <stevemasta34@gmail.com>
2014-11-18 09:02:32 -0700

    The initial stubs for the SlotMachine. This feels super procedural

---
+++ lejosSoftware/SlotMachine/src/Starter.java
@@ -0,0 +1,16 @@
+import model.SlotMachine;
+
+/**
+ * Created by Stephen on 11/18/2014.
+ * In project: NXTSoftware
+ */
+public class Starter
+{
+    public static void main( String[] args )
+    {
+        // Create the Slot Machine
+        // Ihe Machine starts the game
+        SlotMachine slotMachine = new SlotMachine();
+        slotMachine.startGame();
+    }
+}
---
+++ lejosSoftware/SlotMachine/src/model/SlotMachine.java
@@ -0,0 +1,40 @@
+package model;
+
+/**
+ * Created by Stephen on 11/18/2014.
+ * In project: NXTSoftware
+ */
+public class SlotMachine
+{
+    public void startGame()
+    {
+        // Spawn program representation of the following:
+            // Lever, which is composed of a touch sensor and event trigger
+            // Coin (touch) Sensor, that notifies on coin insertion
+        // There is a button press that determines if the USER will play
+            // if user is playing
+                // Decider, which decides how many coins are dumped based on the returned P-value
+            // otherwise the user is not playing and we wait for the next user
+    }
+
+    private static class Decider
+    {
+        // Has a set group of possible actions to take
+        private enum ActionToTake
+        {
+            ReturnSingleCoin, ReturnJackpot, ReturnNothing
+        }
+
+        // Will decide what action to take based on calculated probability
+        private ActionToTake calculate()
+        {
+            double v = Math.random() * 10 + 1;
+            int determinant = ( (int) Math.floor(v) );
+
+            if ( determinant < 4 ) return ActionToTake.ReturnSingleCoin;
+            else if ( determinant > 3 && determinant < 5 ) return ActionToTake.ReturnJackpot;
+            else return ActionToTake.ReturnNothing;
+        }
+
+    }
+}
